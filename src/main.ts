67ae82b02ea0cf1dee197f430fb191c74d5e64b8
= <98428867+yveslevi@users.noreply.github.com>
2023-05-01 02:12:08 -0300

    Adição Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('LearnLink Endpoints')
+    .setDescription('This is the routes that make up the backend of LearnLink application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(5500);
 }
 bootstrap();
